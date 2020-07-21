b8ccb8f42387159f4d0cf63f2e1686b4ba0d4661
Bhavin <bhavinshah20@live.com>
2020-07-21 18:55:48 +0530

    First commit

---
+++ src/main/java/restAPIframework/App.java
@@ -0,0 +1,13 @@
+package restAPIframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
